7c645f228d8991f9ff06566d8112e7dffefdb556
jerry <zhangyang0707@126.com>
2021-05-23 09:11:15 +0800

    项目初始化

---
+++ MultiDataSoruceDemo/src/main/java/com/example/demo/config/DataSourcesConfig.java
@@ -0,0 +1,63 @@
+package com.example.demo.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceBuilder;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
+import com.alibaba.druid.support.spring.stat.DruidStatInterceptor;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.JdkRegexpMethodPointcut;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+
+@Configuration
+public class DataSourcesConfig {
+
+
+
+    @Bean
+    public ServletRegistrationBean druidServlet() { // 主要实现WEB监控的配置处理
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(
+                new StatViewServlet(), "/druid/*"); // 现在要进行druid监控的配置处理操作
+        servletRegistrationBean.addInitParameter("allow", "127.0.0.1,10.1.1.1"); // 白名单
+        // servletRegistrationBean.addInitParameter("deny", "192.168.1.200"); // 黑名单
+        servletRegistrationBean.addInitParameter("loginUsername", "admin"); // 用户名
+        servletRegistrationBean.addInitParameter("loginPassword", "admin"); // 密码
+        servletRegistrationBean.addInitParameter("resetEnable", "false"); // 是否可以重置数据源
+        return servletRegistrationBean ;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean() ;
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*"); // 所有请求进行监控处理
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.css,/druid/*");
+        return filterRegistrationBean ;
+    }
+
+    @Bean(name = "dbOne")
+    @ConfigurationProperties(prefix = "spring.datasource.druid.one")
+    @Primary
+    DataSource dbOne(){
+        return DruidDataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "dbTwo" )
+    @ConfigurationProperties(prefix = "spring.datasource.druid.two")
+    DataSource dbTwo(){
+        return DruidDataSourceBuilder.create().build();
+    }
+
+
+
+}
